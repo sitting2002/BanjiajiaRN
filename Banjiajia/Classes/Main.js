9509862339d5e567a65c7ec00bfd5ca9472f720c
Decade <sitting2002@126.com>
2016-08-27 18:00:10 +0800

    add resours

--- Banjiajia/Classes/Main.js
+++ Banjiajia/Classes/Main.js
@@ -6,22 +6,38 @@
  import{
  	StyleSheet,
  	View,
- 	Text,
+	Text,
+	Image,
  } from 'react-native';
 
- import TabBar from 'react-native-tab-navigator';
+ import TabNavigator from 'react-native-tab-navigator';
 
  export default class Main extends React.Component {
     render() {
       return (
       	<View style = {{flex : 1}}>
 	      	<Text>hello</Text>
-	      	<TabBar style = {Styles.tabbarStyle}>
-      		
-					</TabBar>
+	      	<TabNavigator style = {Styles.tabbarStyle}>
+						{this.setupTabbar('one', './res/icon_tabbar_homepage_selected.png')}
+						{this.setupTabbar('twe', './res/icon_tabbar_homepage_selected.png')}
+						{this.setupTabbar('three')}
+						{this.setupTabbar('four')}
+					</TabNavigator>
       	</View>  
       );
-    }
+    };
+
+		setupTabbar(title, iconNormal){
+			return(
+				<TabNavigator.Item 
+					title = {title} 
+					renderIcon = {() => <Image source = {require(iconNormal)} />}
+					// renderIcon={() => <Image source={{uri:iconName}} style={styles.tabbarIconStyle}/>}
+					                    // <Image source = {require('./my-icon.png')} />
+					>
+				</TabNavigator.Item>
+			);
+		}
   }
 
 const Styles = StyleSheet.create({
