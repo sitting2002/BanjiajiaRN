ffd5e566fdebc5296dec35955367b18aaab977a6
Decade <sitting2002@126.com>
2016-08-27 09:40:19 +0800

    add tabbar phase 1

--- Banjiajia/Classes/Main.js
+++ Banjiajia/Classes/Main.js
@@ -9,15 +9,21 @@
  	Text,
  } from 'react-native';
 
- import TabNavi from 'react-native-tab-navigator';
+ import TabBar from 'react-native-tab-navigator';
 
  export default class Main extends React.Component {
     render() {
       return (
-      	<View>
-      		<Text>hello</Text>
+      	<View style = {{flex : 1}}>
+	      	<Text>hello</Text>
+	      	<TabBar style = {Styles.tabbarStyle}>
+      		
+					</TabBar>
       	</View>  
       );
     }
   }
 
+const Styles = StyleSheet.create({
+	tabbarStyle : {},
+});
