9225ae551b1b008661e9f53661a22eb1d5e9bcd8
Decade <sitting2002@126.com>
2016-08-29 14:34:29 +0800

    add tabbar phase 2

--- Banjiajia/Classes/Main.js
+++ Banjiajia/Classes/Main.js
@@ -8,39 +8,64 @@
  	View,
 	Text,
 	Image,
+	Platform,
  } from 'react-native';
 
  import TabNavigator from 'react-native-tab-navigator';
 
- export default class Main extends React.Component {
-    render() {
-      return (
-      	<View style = {{flex : 1}}>
-	      	<Text>hello</Text>
-	      	<TabNavigator style = {Styles.tabbarStyle}>
-						{this.setupTabbar('one', './res/icon_tabbar_homepage_selected')}
-						{this.setupTabbar('twe', './res/icon_tabbar_homepage_selected.png')}
-						{this.setupTabbar('three')}
-						{this.setupTabbar('four')}
-					</TabNavigator>
-      	</View>  
-      );
-    };
+const Tab0 = 'tab0'
+const Tab1 = 'tab1'
+const Tab2 = 'tab2'
+const Tab3 = 'tab3'
 
-		setupTabbar(title, iconNormal){
-			return(
-				<TabNavigator.Item 
-					title = {title} 
-					renderIcon = {() => <Image source = {{uri:iconNormal}}/>}
-					// <Image source={require('./my-icon.png')} />
-					// renderIcon = {() => <Image source = {{uri:iconNormal}} />}
-					// renderIcon={() => <Image source={{uri:iconName}} style={styles.tabbarIconStyle}/>}
-					>
-				</TabNavigator.Item>
-			);
-		}
+ export default class Main extends React.Component {
+	constructor(props) {
+		super(props);
+            this.state = {selectedTab : Tab0}
+	}
+
+	setupTabbar(title, iconNormal, iconSelect, selectTab, childView){
+		return(			
+			<TabNavigator.Item 
+				title = {title} 
+				renderIcon = {() => <Image source = {{uri:iconNormal}} style = {Styles.tabbarIconStyle}/>}
+				renderSelectedIcon = {() => <Image source = {{uri:iconSelect}} style = {Styles.tabbarIconStyle}/>}
+				selected = {this.state.selectedTab === selectTab}
+				onPress = {() => this.setState({selectedTab : selectTab})}
+				>
+				{childView}
+			</TabNavigator.Item>
+		);
+	}
+
+	static _createChildView(selectTab) {
+        return (
+            <View style={{flex:1,backgroundColor:'#00baff',alignItems:'center',justifyContent:'center'}}>
+                <Text style={{fontSize:22}}>{selectTab}</Text>
+            </View>
+        )
+    }
+
+    render() {
+		return (
+			<View style = {{flex : 1}}>
+				<TabNavigator style = {Styles.tabbarStyle}>
+					{this.setupTabbar('one', 'icon_tabbar_merchant_normal', 'icon_tabbar_merchant_selected', Tab0, Main._createChildView(Tab0))}
+					{this.setupTabbar('twe', 'icon_tabbar_homepage', 'icon_tabbar_homepage_selected', Tab1, Main._createChildView(Tab1))}
+					{this.setupTabbar('three', 'icon_tabbar_mine', 'icon_tabbar_mine_selected', Tab2, Main._createChildView(Tab2))}
+					{this.setupTabbar('four', 'icon_tabbar_misc', 'icon_tabbar_misc_selected', Tab3, Main._createChildView(Tab3))}
+				</TabNavigator>
+			</View>  
+		);
+	}
   }
 
 const Styles = StyleSheet.create({
-	tabbarStyle : {},
+	tabbarStyle : {
+
+	},
+	tabbarIconStyle:{
+        width: Platform.os === 'ios' ? 30 : 25,
+        height:Platform.os === 'ios' ? 30 : 25
+    },
 });
