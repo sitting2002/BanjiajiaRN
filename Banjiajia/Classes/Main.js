3cafd46c952ba6b2c6cb1c4beec35ca5791d3cdf
Decade <sitting2002@126.com>
2016-08-29 09:24:59 +0800

    add some img

--- Banjiajia/Classes/Main.js
+++ Banjiajia/Classes/Main.js
@@ -18,7 +18,7 @@
       	<View style = {{flex : 1}}>
 	      	<Text>hello</Text>
 	      	<TabNavigator style = {Styles.tabbarStyle}>
-						{this.setupTabbar('one', './res/icon_tabbar_homepage_selected.png')}
+						{this.setupTabbar('one', './res/icon_tabbar_homepage_selected')}
 						{this.setupTabbar('twe', './res/icon_tabbar_homepage_selected.png')}
 						{this.setupTabbar('three')}
 						{this.setupTabbar('four')}
@@ -31,9 +31,10 @@
 			return(
 				<TabNavigator.Item 
 					title = {title} 
-					renderIcon = {() => <Image source = {require(iconNormal)} />}
+					renderIcon = {() => <Image source = {{uri:iconNormal}}/>}
+					// <Image source={require('./my-icon.png')} />
+					// renderIcon = {() => <Image source = {{uri:iconNormal}} />}
 					// renderIcon={() => <Image source={{uri:iconName}} style={styles.tabbarIconStyle}/>}
-					                    // <Image source = {require('./my-icon.png')} />
 					>
 				</TabNavigator.Item>
 			);
